4f1df7608820d83e017a51e27ec0c44fa4f45134
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-05-16 16:25:22 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,10 @@ Main file
 """
 
 import re
+import mysql.connector
+from os import getenv
 from typing import List
+import logging
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -19,8 +22,8 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
 
 
 class RedactingFormatter(logging.Formatter):
-    """
-    Redacting Formatter class
+    """ 
+        RedactingFormatter class
     """
 
     REDACTION = "***"
@@ -28,12 +31,64 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """Constructor Method"""
-        super().__init__(self.FORMAT)
+        """ Constructor Method """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Filters values in incoming log records using filter_datum"""
+        """ Filters values in incoming log records using filter_datum """
         return filter_datum(self.fields, self.REDACTION,
-                            super().format(record),
+                            super(RedactingFormatter, self).format(record),
                             self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    ''' 
+        logger function
+    '''
+    log = logging.getLogger('user_data')
+    log.setLevel(logging.INFO)
+    log.propagate = False
+
+    sh = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    sh.setFormatter(formatter)
+    log.addHandler(sh)
+
+    return log
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    ''' 
+        db function
+    '''
+    connection_db = mysql.connector.connection.MySQLConnection(
+        user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=getenv('PERSONAL_DATA_DB_NAME'))
+
+    return connection_db
+
+
+def main():
+    '''
+        main function
+    '''
+    database = get_db()
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = [i[0] for i in cursor.description]
+
+    log = get_logger()
+
+    for row in cursor:
+        str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, fields))
+        log.info(str_row.strip())
+
+    cursor.close()
+    database.close()
+
+
+if __name__ == '__main__':
+    main()

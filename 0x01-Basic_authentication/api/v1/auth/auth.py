17d86d3878bb9e014573c9ed4fdd8b94c8206ef9
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-22 00:17:31 +0100

    chore: Refactor require_auth method to handle excluded paths correctly

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-Module of Auth views
+"""Authentication module.
 """
 from flask import request
 from typing import List, TypeVar
@@ -8,29 +7,31 @@ import fnmatch
 
 
 class Auth:
-    """ Auth class
+    """Authentication class.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-    """ require_auth
-    """
-    if path is None:
+        """ Method to check if auth is required.
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
         return True
-
-    if excluded_paths is None or not excluded_paths:
-        return True
-
-    for excluded_path in excluded_paths:
-        if fnmatch.fnmatch(path, excluded_path):
-            return False
-
-    return True
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header
+        """ Method to get authorization header.
         """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current_user
+        """ Method to get user from request.
         """
         return None

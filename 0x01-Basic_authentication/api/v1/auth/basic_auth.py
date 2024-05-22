30d54a9f4748aacff2f2a64ef817126692dc1d39
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-22 01:36:24 +0100

    Refactor BasicAuth class to improve user_object_from_credentials method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,3 +45,19 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ user_object_from_credentials
+        """
+        if user_email is None or user_pwd is None:
+            return None
+        if type(user_email) is not str or type(user_pwd) is not str:
+            return None
+        try:
+            user = User.search({'email': user_email})
+        except Exception:
+            return None
+        if user is None or not user.is_valid_password(user_pwd):
+            return None
+        return user

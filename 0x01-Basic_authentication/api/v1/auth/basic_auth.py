1fe9d57669336bd27a47fefe5f2fb31c364743d1
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-22 01:32:10 +0100

    Refactor BasicAuth class to handle base64 authorization header correctly

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,9 +13,9 @@ class BasicAuth(Auth):
             self, authorization_header: str) -> str:
         """ extract_base64_authorization_header
         """
-        if base64_authorization_header is None:
+        if authorization_header is None:
             return None
-        if type(base64_authorization_header) is not str:
+        if type(authorization_header) is not str:
             return None
         if authorization_header[:6] != 'Basic ':
             return None

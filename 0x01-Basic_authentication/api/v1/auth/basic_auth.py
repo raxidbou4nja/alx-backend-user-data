2710b63abd0fb3cac2694c44860464b0daac6b22
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-22 01:41:19 +0100

    Refactor BasicAuth class to add current_user method for retrieving user from request

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -64,3 +64,12 @@ class BasicAuth(Auth):
         if user is None or not user.is_valid_password(user_pwd):
             return None
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        user_email, user_pwd = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(user_email, user_pwd)

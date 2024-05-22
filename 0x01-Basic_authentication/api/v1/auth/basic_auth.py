2c943342039a52e41bc02e6789ce638dbcff1abb
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-22 01:22:39 +0100

    Refactor BasicAuth class to extract and decode base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,9 +9,10 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth class
     """
-    def decode_base64_authorization_header(
-            self, base64_authorization_header: str) -> str:
-        """ extract_base64_authorization_header """
+    def extract_base64_authorization_header(self,
+            authorization_header: str) -> str:
+        """ extract_base64_authorization_header
+        """
         if authorization_header is None or type(authorization_header) is not str:
             return None
         if authorization_header[:6] != 'Basic ':

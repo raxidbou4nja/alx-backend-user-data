0912779bdc3a0fbe7e021e370fe203d8e59f6791
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-23 15:59:57 +0100

    chore: Refactor UserSession model for session authentication

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,34 +1,20 @@
 #!/usr/bin/env python3
-"""
-User session module
+""" module
 """
 from models.base import Base
-from models.user import User
-from typing import TypeVar
-import hashlib
+from typing import TypeVar, List, Iterable
+from os import path
+import json
 import uuid
 
 
 class UserSession(Base):
-    """ UserSession class
+    """ class
     """
 
     def __init__(self, *args: list, **kwargs: dict):
         """ Initialize a UserSession instance
         """
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id', "")
-        self.session_id = kwargs.get('session_id', "")
-        self.session_token = kwargs.get('session_token', "")
-
-    def create_session_token(self) -> str:
-        """ Create a session token
-        """
-        token = str(uuid.uuid4())
-        self.session_token = hashlib.md5(token.encode()).hexdigest()
-        return self.session_token
-
-    def get_user(self) -> TypeVar('User'):
-        """ Get the User instance
-        """
-        return User.get(self.user_id)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')

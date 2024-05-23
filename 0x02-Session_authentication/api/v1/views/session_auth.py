67e3dd6d94736ff68147ef354599e64552b32b37
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-23 15:42:59 +0100

    chore: Add session authentication views for login and logout

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Module of SessionAuth views
+"""
+from api.v1.views import app_views
+from models.user import User
+import uuid
+from flask import request, jsonify
+from api.v1.auth.session_auth import SessionAuth
+
+sa = SessionAuth()
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /api/v1/auth_session/login
+    Return:
+      - the status of the API
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return jsonify({"error": "email or password missing"}), 400
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    session_id = sa.create_session(user[0].id)
+    response = jsonify(user[0].to_json())
+    response.set_cookie('session_id', session_id)
+    return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - the status of the API
+    """
+    if sa.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)

7dc71eaffc1e8d099b928375e6af4faf61fab654
raxidbou4nja <rachid.boughnja@gmail.com>
2024-05-29 23:19:56 +0100

    chore: Add User model for user authentication service

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+User model
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    User class
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __str__(self):
+        """ String representation of the user
+        """
+        return self.email
+
+    def __repr__(self):
+        """ String representation of the user
+        """
+        return self.email
